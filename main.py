4526475c164749af55f0b27beda6efd99cb3f4e5
DR-Vora <voradhairya22@gmail.com>
2024-12-04 23:41:34 +0530

    Newspaper Reader is ready with basic functinality. Will only read tech related news right now but updates are on the way...

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pyttsx3
+import requests
+import json
+
 e = pyttsx3.init('sapi5')
 
 v = e.getProperty('voices')
@@ -8,4 +11,23 @@ def speak(audio):
     e.runAndWait()
 
 if __name__=="__main__":
-    speak("Hello, I am your assistant")
+    speak("Hello, I am your assistant .And i am going to read news for you. Now the 1st News is by")
+
+    url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=7e7a3d0035594f3686e9c934997f7ad3"
+
+    news = requests.get(url).text
+    news = json.loads(news)
+
+    #print(news["totalResults"])
+
+    arts = news['articles']
+
+    for article in arts:
+        print(article['author'])
+        speak(article['author'])
+        print(article['title'])
+        speak(article['title'])
+        print(article['description'])
+        speak(article['description'])
+        speak("Moving to the next news which is by ")
+        
